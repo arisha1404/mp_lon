d4ab3e44acad5aeb869d6a56c9d9e1d6dc3a3fe4
Арина Лонщакова <arina14041@outlook.com>
2021-07-04 09:45:54 +0300

    Добавьте файлы проекта.

---
+++ mp/BaseBd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mp
+{
+    class BaseBd
+    {
+        public static Entities B;
+    }
+}
---
+++ mp/Fr.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace mp
+{
+    class Fr
+    {
+        public static Frame F;
+    }
+}
